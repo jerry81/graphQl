601631d9e8cb887694ff394c69161632d9f28206
Raskolnikov <jerry.tann@opteyes.cn>
2021-04-16 13:30:29 +0800

    feat: add server graph ql

--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
 const app = express()
 const port = 3001
 
@@ -7,6 +9,21 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+ 
+var root = { hello: () => 'Hello world!' };
+ 
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
+  
